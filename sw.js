60ca99cb7cd2aea563bb141dc0efe859731f481c
Yuri Possimozer <114203188+YuriPGamer@users.noreply.github.com>
2022-12-25 21:26:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
+const CACHE_NAME = 'cool-cache';
 
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/Assets/',
+    '/Letras/',
+    '/Styles/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
+});
+
+// Claiming Clients During the Activate Event.
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+// Defining A Fetch Strategy.
+
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
+});
