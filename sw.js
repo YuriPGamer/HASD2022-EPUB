36e435eb2679495ccab845073dc1a0223d7d90f4
Yuri Possimozer <114203188+YuriPGamer@users.noreply.github.com>
2022-12-25 21:34:40 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -14,29 +14,3 @@ self.addEventListener('install', event => {
         cache.addAll(PRECACHE_ASSETS);
     })());
 });
-
-// Claiming Clients During the Activate Event.
-
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-
-// Defining A Fetch Strategy.
-
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
-
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
-
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
-});
